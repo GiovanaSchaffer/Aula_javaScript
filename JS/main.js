2b7c457278e5a87b5250355c83c9cd986e79e86a
GiovanaSchaffer <giovanamoreira12@gmail.com>
2021-02-16 11:00:08 -0300

    terminado

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,101 @@
-alert("Meu segundo JS");
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+    //alert("Obrigado por clicar!");
+}
+
+function redirecionar(){
+    window.open("https://www.cursoemvideo.com/");
+}
+
+
+/*function soma(n1,n2){
+    return n1+n2;
+}
+
+//alert(soma(18,10));
+
+function validaIdade(idade){
+    if(idade>=18){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+var idade = prompt("Qual a sua idade: ");
+console.log(validaIdade(idade));
+*/
+
+/*var d = new Date();
+//alert(d);
+alert(d.getMonth()+1);
+*/
+
+/*var count;
+for(count = 1; count <=5; count++){
+    alert(count);
+}*/
+
+/*var count = 0;
+while(count<=5){
+    console.log(count);
+    count+=1;
+}*/
+
+/*var idade = prompt("Qual a sua idade: ");
+
+console.log(idade);
+
+if (idade>=18){
+    alert("Maior de idade");
+}else{
+    alert("Menor de idade");
+}*/
+
+/*var frutas = [
+    {nome: "Maça", cor: "vermelha"},
+    {nome: "pêra", cor: "verde"},
+    {nome: "melância", cor: "vermelho e verde"}
+];
+
+console.log(frutas[1].cor);
+*/
+
+
+/*var fruta = {
+    nome : "maça",
+    cor : "vermelha"
+};
+
+console.log(fruta.nome);
+console.log(fruta.cor);
+console.log(fruta);
+alert(fruta.cor);
+*/
+
+/*var lista = ["maça", "pêra", "laranja"];
+console.log(lista);
+console.log(lista[1]);
+lista.pop();
+console.log(lista);
+lista.push("uva");
+console.log(lista);
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(" - "));
+*/
+
+
+/*var nome = "Giovana Schaffer";
+var idade = 19;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+alert(nome + " tem" + idade + " anos");
+alert(idade + idade2);
+console.log(nome);
+console.log(idade2+idade);
+console.log(frase.replace("Japão", "Brasil"));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());*/
+
